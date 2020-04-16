3c92e00b928549206da6ff426a1b314208bcd9a1
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-04-16 13:14:32 -0500

    finished pert 2

---
+++ student_exercises/nss_items/__init__.py
@@ -0,0 +1,5 @@
+from nss_items.cohort import Cohort
+from nss_items.exercise import Exercise
+from nss_items.instructor import Instructor
+from nss_items.student import Student
+from nss_items.nss_person import NSSPerson
